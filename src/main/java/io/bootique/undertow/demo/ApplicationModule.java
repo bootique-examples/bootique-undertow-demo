65b3c43a1170f9282f80c29b777e8ba0034d50f1
Dmitriy Tarasenko <dmitriy.tarasienko@gmail.com>
2021-06-03 15:40:40 +0300

    feat: upgrade to 2.0.B1 and move to separate branch

--- src/main/java/io/bootique/undertow/demo/ApplicationModule.java
+++ src/main/java/io/bootique/undertow/demo/ApplicationModule.java
@@ -5,8 +5,6 @@ import io.bootique.di.Binder;
 import io.bootique.undertow.handlers.RootHandler;
 import io.undertow.server.HttpHandler;
 
-import javax.inject.Singleton;
-
 /**
  * @author Ibragimov Ruslan
  */
@@ -15,7 +13,7 @@ public class ApplicationModule implements BQModule {
     public void configure(Binder binder) {
 
         // Provide our HttpHandler as implementation of root http handler for undertow.
-        binder.bind(HttpHandler.class)
-            .toProvider(RootHttpHandler.class).inSingletonScope();
+        binder.bind(HttpHandler.class, RootHandler.class)
+                .toProvider(RootHttpHandler.class).inSingletonScope();
     }
 }
